6231ea07daa7a30912c6bded3deb56826e2959af
MichaelSun <zhensherlock@126.com>
2022-12-06 18:16:07 +0800

    docs: update images

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-## 0.0.1 (2022-12-05)
+## [0.0.1](https://github.com/zhensherlock/watermark-js-plus/compare/fb530a78a35020947fec230142a131b56b3588b4...v0.0.1) (2022-12-05)
 
 
 ### ✨ Features | 新功能
--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -6,8 +6,8 @@ export default {
   description: 'A watermark plugin',
   base: '/watermark-js-plus',
   head: [
-    ['link', { rel: 'shortcut icon', href: '/favicons/favicon-64x64.png' }],
-    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicons/apple-touch-icon.png' }],
+    ['link', { rel: 'shortcut icon', href: '/watermark-js-plus/favicons/favicon-64x64.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/watermark-js-plus/favicons/apple-touch-icon.png' }],
     // ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicons/favicon-32x32.png' }],
     // ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicons/favicon-16x16.png' }],
     // ['link', { rel: "manifest", href: "/assets/favicons/site.webmanifest"}],
