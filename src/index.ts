b97e864297ee8eaaa0f32b12c2cc0c6368323dde
Mr.Hope <mister-hope@outlook.com>
2024-05-08 01:16:22 +0800

    feat: export types

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
-import { Watermark } from './core/watermark'
-import { BlindWatermark } from './core/blind'
-import { ImageWatermark } from './core/image'
 import './style'
 
-export {
-  Watermark,
-  BlindWatermark,
-  ImageWatermark
-}
+export { Watermark } from './core/watermark'
+export { BlindWatermark } from './core/blind'
+export { ImageWatermark } from './core/image'
+export type * from './types'
