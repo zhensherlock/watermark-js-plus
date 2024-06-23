50ece22d877807318dd1f56335874ca21f9f0ae2
MichaelSun <zhensherlock@126.com>
2024-06-23 14:04:03 +0800

    test: update create BlindWatermark test

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  testEnvironmentOptions: {
+    resources: 'usable'
+  },
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageReporters: ['json', 'lcov', 'text', 'clover']
