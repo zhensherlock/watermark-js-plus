337a9536b9d8932564cc1891eab1ef85f6b1c44e
MichaelSun <zhensherlock@126.com>
2024-06-26 15:23:10 +0800

    test: update watermark test

--- tests/core/image.test.ts
+++ tests/core/image.test.ts
@@ -7,30 +7,6 @@ import { sleep } from '../utils'
 const imageWatermarkOriginal = 'https://upic-1258271354.cos.ap-shanghai.myqcloud.com//uPic/image-watermark-original-tuxD08.jpg'
 
 describe('core image module', () => {
-  // test('image-watermark create expected true', (done) => {
-  //   document.body.innerHTML = `<img class="text-watermark-image" src="${imageWatermarkOriginal}">`
-  //
-  //   setTimeout(async () => {
-  //     const imgDom = <HTMLImageElement>$('.text-watermark-image').get(0)
-  //     const watermark = new ImageWatermark({
-  //       content: 'my text watermark',
-  //       width: imgDom.width,
-  //       height: imgDom.height,
-  //       dom: imgDom,
-  //       rotate: 0,
-  //       translatePlacement: 'bottom-end',
-  //       fontColor: '#fff',
-  //       globalAlpha: 0.5,
-  //       fontSize: '30px'
-  //     })
-  //     await watermark.create()
-  //     done()
-  //     expect($('.text-watermark-image').attr('src')).toBe(imageWatermarkTextResult)
-  //     watermark.destroy()
-  //     expect($('.text-watermark-image').attr('src')).toBe(imageWatermarkOriginal)
-  //   }, 10000)
-  // }, 600000)
-
   test('image-watermark create expected true', async () => {
     document.body.innerHTML = `<img class='text-watermark-image' src='${imageWatermarkOriginal}'>`
 
--- tests/core/watermark.test.ts
+++ tests/core/watermark.test.ts
@@ -6,6 +6,7 @@ describe('core watermark module', () => {
   test('Watermark create expected true', async () => {
     const watermark = new Watermark({
       content: 'hello my watermark',
+      textType: 'stroke',
       width: 200,
       height: 200,
       rotate: 22,
@@ -29,9 +30,40 @@ describe('core watermark module', () => {
           }
         ]
       },
+      extraDrawFunc: () => {},
       zIndex: 2147483646
     })
     await watermark.create()
+    await watermark.changeOptions({
+      auxiliaryLine: true,
+      contentType: 'rich-text',
+      content: 'multi text watermark',
+      textType: 'stroke',
+      shadowStyle: {
+        shadowBlur: 10,
+        shadowColor: '#000000FF',
+        shadowOffsetX: 0,
+        shadowOffsetY: 0
+      },
+      advancedStyle: {
+        type: 'radial',
+        params: {
+          radial: {
+            x0: 0,
+            y0: 0,
+            r0: 0,
+            x1: 0,
+            y1: 0,
+            r1: 0
+          }
+        },
+        colorStops: [
+          { offset: 0, color: 'red' },
+          { offset: 0.5, color: 'green' },
+          { offset: 1, color: 'blue' }
+        ]
+      }
+    }, 'append')
     expect($('body > div:last').css('z-index')).toBe('2147483646')
     watermark.destroy()
     expect($('body > div:last').css('z-index')).toBe(undefined)
