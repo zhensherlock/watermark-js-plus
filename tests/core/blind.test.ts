43cc4619c11524290448c4393d906bca336f54f9
MichaelSun <zhensherlock@126.com>
2025-02-13 14:24:42 +0800

    test: update blind watermark test function

--- tests/core/blind.test.ts
+++ tests/core/blind.test.ts
@@ -17,8 +17,11 @@ describe('core blind module', () => {
       textBaseline: 'bottom'
     })
     await watermark.create()
-    await watermark.changeOptions()
     expect($('body > div:last').css('z-index')).toBe('2147483646')
+    await watermark.changeOptions({
+      zIndex: 2147483645
+    })
+    expect($('body > div:last').css('z-index')).toBe('2147483645')
     watermark.destroy()
     expect($('body > div:last').css('z-index')).toBe(undefined)
   })
